d7286cc281e9ca054d330dacc5cf43f6427c35b4
Andre <andrey.bone2@gmail.com>
2025-07-24 17:04:41 +0200

    fix: correct webhook URL

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# вначале файла
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def on_startup(bot: Bot):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    # 1) Railway проставит переменную RAILWAY_STATIC_URL
     base = os.getenv("RAILWAY_STATIC_URL", f"localhost:{os.getenv('PORT', 8080)}")
     webhook_url = f"https://{base}/{BOT_TOKEN}"
     await bot.set_webhook(webhook_url)
